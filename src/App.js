29151446c43518af53dc1b57bf1f0d3d8e40aecc
MatiasOvejero <matilmds91@gmail.com>
2021-01-05 13:06:37 -0300

    Wip new input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+
+function ToDoForm(addTodo){
+  const [value,setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <input
+        type = "text"
+        class = "input"
+        value = {value}
+        onChange = {e => setValue(e.target.value)}
+      />
+    </form>
+  )
+}
+
+function Todo({ todo }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  );
+};
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+  
+  const addTodo = text =>{
+    const newTodos = [...todos, {text}];
+    setTodos (newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+        <ToDoForm addTodo = {addTodo}/>
+      </div>
     </div>
   );
 }
